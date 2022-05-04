05d7d4be39ffc0fb34f11c586a1196745740f13b
Nicolas Marquez <nicolas_marquez10@hotmail.com>
2022-05-04 16:42:47 -0300

    MVP working, as far as I know XD

--- endpoint.js
+++ endpoint.js
@@ -2,6 +2,7 @@
 //////////////////////////////////////////////
 //            Endpoints Framework           //
 //////////////////////////////////////////////
+const { logger } = require('slingr-endpoints');
 const endpoint = require('slingr-endpoints');
 
 //////////////////////////////////////////////
@@ -21,10 +22,16 @@ endpoint.hooks.onEndpointStop = (cause) => {
 //////////////////////////////////////////////
 //            Endpoint Functions            //
 //////////////////////////////////////////////
-endpoint.functions.randomNumber = (params) => {
 
-    // this log will be sent to the app and can be seen in the general logs in the app monitor
-    endpoint.appLogger.info("Request to generate random number received", params);
+//Functions receive a request parameter which has some info that the platform adds plus.
+//the arguments sent to the function
+endpoint.functions.randomNumber = (req) => {
+
+    // this log will be sent to the app and can be seen in the general logs in the app monitor.
+    endpoint.appLogger.info('Request to generate random number received', req);
+
+    //This is how we fetch the arguments sent to the function from the slingr app.
+    const params = req.params;
 
     // generate random number
     let max = 10000;
@@ -39,90 +46,100 @@ endpoint.functions.randomNumber = (params) => {
     responseToApp.number = Math.round(Math.random() * max);
 
     // this is an internal log of the endpoint
-    endpoint.logger.info('Random number generated:' + responseToApp.number);
+    endpoint.logger.info('Random number generated: ', responseToApp.number);
 
-    //Here we check if the number has been generated before, if so, we increment its ocurrences added the timestamp. Else, we save it on the dataStore.
-    endpoint.dataStores.dataStore1.findOne({ randomNumber: responseToApp.number }).then((savedRandomNumber) => {
-        if (!savedRandomNumber) {
-            endpoint.dataStores.dataStore1.save({
-                randomNumber: responseToApp.number.toString(),
-                ocurrences: [new Date().getTime().toString()]
-            });
-        } else {
-            savedRandomNumber.ocurrences.push(new Date().getTime().toString());
-            endpoint.dataStores.dataStore1.update(savedRandomNumber._id, savedRandomNumber);
-        }
-    }).catch((err) => {
-        endpoint.logger.error('error while querying the db.');
-        endpoint.appLogger.error('error while querying the db.', err);
-    });
-
+    //Functions should always return a valid JSON or object
     return responseToApp;
 }
 
-endpoint.functions.ping = (req) => {
-    // in this case as the argument is FunctionRequest we don't get only the body of the request,
-    // but all the information associated to it, which is needed when having callbacks
+endpoint.functions.findAndSaveDocument = (req) => {
+    const document = req.params;
+    //Here we search for a doc with the same id. If found, we update it. 
+    //If no id is sent, we save it on the dataStore as a new document.
+    if (document.id) {
+        endpoint.dataStores.dataStore1.findOne({ _id: document.id }).then((savedDoc) => {
+            if (!savedDoc) {
+                const updatedDocument = { ...savedDoc, ...document }
+                endpoint.dataStores.dataStore1.update(updatedDocument.id, updatedDocument).then((updatedDoc) => {
+                    endpoint.logger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
+                    endpoint.appLogger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
+                });
+            } else {
+                endpoint.logger.warn('No document found with id [' + document.id + ']');
+                endpoint.appLogger.warn('No document found with id [' + document.id + ']');
+            }
+        }).catch((err) => {
+            endpoint.logger.error('error while querying the db.');
+            endpoint.appLogger.error('error while querying the db.', err);
+        });
+    } else {
+        document.createdAt = new Date().getTime()
+        endpoint.dataStores.dataStore1.save(document).then((newDocument) => {
+            endpoint.logger.info('Document saved successfully', newDocument);
+            endpoint.appLogger.info('Document saved successfully', newDocument);
+        });
+    }
 
-    // this log will be sent to the app and can be seen in the general logs in the app monitor
-    endpoint.appLogger.info("Request to ping received", req);
+    return { msg: 'ok' };
+}
+
+endpoint.functions.ping = (req) => {
+
+    endpoint.appLogger.info('Request to ping received', req);
 
     let data = req.params;
 
     let res = {};
+    //This is how we access the settings of the endpoint
     res.token = endpoint.settings.token;
-    res.ping = "pong";
+    res.ping = 'pong';
 
-    //send event
-    endpoint.events.send("pong", data, req.id);
+    //send 'pong' event. This will cause either the listener, or the callback to be executed sometime in the future.
+    endpoint.events.send('pong', data, req.id);
 
-    // this is an internal log of the endpoint
-    endpoint.logger.info("Pong sent to app");
+    endpoint.logger.info('Pong sent to app');
 
-    // we will return the status for the function call, while the real response will go in the callback
     return { status: 'ok' };
 };
-endpoint.functions.error = (req) => {
-    endpoint.appLogger.warn("Request to generate error received");
 
-    throw new Error("Error generated!");
+endpoint.functions.error = (req) => {
+    endpoint.appLogger.warn('Request to generate error received');
+
+    throw new Error('Error generated!');
 }
 
 endpoint.functions.downloadFileFromEndpoint = (endpointRequest) => {
-    const parameters = endpointRequest.params;
-    endpoint.files.download(parameters.fileId).then(
+    const file = endpointRequest.params;
+    endpoint.files.download(file.id).then(
         (res) => {
-            logger.info('File download has completed!')
-            endpoint.events.send('testCallback', null, endpointRequest.id)
+            endpoint.logger.info('File download has completed!');
+            //In this case we return res.toString() because we know the file being downloaded is a .txt. Its not recommended to return the plain buffer to the platform.
+            endpoint.events.send('onDownloadComplete', res.toString(), endpointRequest.id)
         }
     );
-    return { msg: 'file is being downloaded' }
+    return { msg: 'File [' + file.id + '] is being downloaded.' }
 };
 
 endpoint.functions.uploadFileFromEndpoint = (endpointRequest) => {
-    try {
-        endpoint.httpModule.get('https://jsoncompare.org/LearningContainer/SampleFiles/PDF/sample-pdf-with-images.pdf',
-            {
-                responseType: "arraybuffer"
-            }).then(
-                (res) => {
-                    endpoint.files.upload('somefile.pdf', res.data)
-                        .then((fileResponse) => {
-                            endpoint.events.send('onUploadFinished', fileResponse, endpointRequest.id);
-                        });
-                }
-            );
-    } catch (error) {
-        console.error("el error es: ", error);
-    }
+    const fileUrl = 'https://jsoncompare.org/LearningContainer/SampleFiles/PDF/sample-pdf-with-images.pdf'; 
+    endpoint.httpModule.get(fileUrl).then( res => {
+                endpoint.files.upload('somefile.pdf', res.data)
+                    .then((fileResponse) => {
+                        endpoint.events.send('onUploadComplete', fileResponse, endpointRequest.id);
+                    });
+        }).catch( error => {
+            endpoint.logger.error('Couldn\'t download the file from ['+fileUrl+'].',error);
+        });
 
     return { msg: 'file is being downloaded' }
 };
 
+
 endpoint.functions.executeScript = (endpointRequest) => {
-    const {script, parameters} = endpointRequest.params;
-    
-    endpoint.scripts.execute();
+    const { scriptName, parameters } = endpointRequest.params;
+
+    const res = endpoint.scripts.execute(scriptName, parameters);
+    return {msg: 'Script ['+scriptName+'] will be executed shortly in a separate job',response: res};
 }
 
 //////////////////////////////////////////////
@@ -136,14 +153,14 @@ endpoint.webServices.webhooks = {
         try {
             body = req.body;
         } catch (err) {
-            throw new Error("Body must be valid JSON");
+            throw new Error('Body must be valid JSON');
         }
 
         // send event to app
-        endpoint.events.send("inboundEvent", body);
+        endpoint.events.send('inboundEvent', body);
 
         // this is what the webhook caller receives as response
-        res.send({ status: "ok" });
+        res.send({ status: 'ok' });
     }
 }
 
---
+++ scripts/exampleScriptToBeExecuted.js
@@ -0,0 +1,2 @@
+sys.logs.warn("FROM SCRIPT EXECUTION!!!")
+sys.logs.info("We also have access to context: ",JSON.stringify(context));
