7d70b3468a06425c3322e43b6230cdcc7081061d
Nicolas Marquez <nicolas_marquez10@hotmail.com>
2022-05-12 20:11:23 -0300

    Updated functions to cover more use cases

--- endpoint.js
+++ endpoint.js
@@ -21,11 +21,7 @@ endpoint.hooks.onEndpointStop = (cause) => {
 //////////////////////////////////////////////
 //            Endpoint Functions            //
 //////////////////////////////////////////////
-
-//Functions receive a request parameter which has some info that the platform adds plus.
-//the arguments sent to the function
 endpoint.functions.randomNumber = (endpointRequest) => {
-
     // this log will be sent to the app and can be seen in the general logs in the app monitor.
     endpoint.appLogger.info('Request to generate random number received', endpointRequest);
 
@@ -51,39 +47,8 @@ endpoint.functions.randomNumber = (endpointRequest) => {
     return responseToApp;
 }
 
-endpoint.functions.findAndSaveDocument = (endpointRequest) => {
-    const document = endpointRequest.params;
-    //Here we search for a doc with the same id. If found, we update it. 
-    //If no id is sent, we save it on the dataStore as a new document.
-    if (document.id) {
-        endpoint.dataStores.dataStore1.findOne({ _id: document.id }).then((savedDoc) => {
-            if (!savedDoc) {
-                const updatedDocument = { ...savedDoc, ...document }
-                endpoint.dataStores.dataStore1.update(updatedDocument.id, updatedDocument).then((updatedDoc) => {
-                    endpoint.logger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
-                    endpoint.appLogger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
-                });
-            } else {
-                endpoint.logger.warn('No document found with id [' + document.id + ']');
-                endpoint.appLogger.warn('No document found with id [' + document.id + ']');
-            }
-        }).catch((err) => {
-            endpoint.logger.error('error while querying the db.');
-            endpoint.appLogger.error('error while querying the db.', err);
-        });
-    } else {
-        document.createdAt = new Date().getTime()
-        endpoint.dataStores.dataStore1.save(document).then((newDocument) => {
-            endpoint.logger.info('Document saved successfully', newDocument);
-            endpoint.appLogger.info('Document saved successfully', newDocument);
-        });
-    }
-
-    return { msg: 'ok' };
-}
 
 endpoint.functions.ping = (endpointRequest) => {
-
     endpoint.appLogger.info('Request to ping received', endpointRequest);
 
     let data = endpointRequest.params;
@@ -107,7 +72,39 @@ endpoint.functions.error = (endpointRequest) => {
     throw new Error('Error generated!');
 }
 
-endpoint.functions.downloadFileFromEndpoint = (endpointRequest) => {
+endpoint.functions.findAndSaveDocument = async (endpointRequest) => {
+    const document = endpointRequest.params;
+    //Here we search for a doc with the same id. If found, we update it. 
+    //If no id is sent, we save it on the dataStore as a new document.
+    let docResponse;
+    if (document.id) {
+        let existingDoc;
+        try {
+            existingDoc = await endpoint.dataStores.dataStore1.findOne({ _id: document.id });
+        } catch (error) {
+            endpoint.logger.error('error while querying the datastore.');
+            endpoint.appLogger.error('error while querying the datastore.', err);
+        }
+        if (!existingDoc) {
+            const updatedDocument = { ...savedDoc, ...document };
+            docResponse = await endpoint.dataStores.dataStore1.update(updatedDocument.id, updatedDocument);
+            endpoint.logger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
+            endpoint.appLogger.info('Document [' + document.id + '] updated succesfully', updatedDoc);
+        } else {
+            endpoint.logger.warn('No document found with id [' + document.id + ']');
+            endpoint.appLogger.warn('No document found with id [' + document.id + ']');
+        }
+    } else {
+        document.createdAt = new Date().getTime()
+        docResponse = await endpoint.dataStores.dataStore1.save(document);
+        endpoint.logger.info('Document saved successfully', newDocument);
+        endpoint.appLogger.info('Document saved successfully', newDocument);
+    }
+
+    return docResponse;
+}
+
+endpoint.functions.asyncDownloadFileFromEndpoint = async (endpointRequest) => {
     const file = endpointRequest.params;
     endpoint.files.download(file.id).then(
         (res) => {
@@ -116,29 +113,62 @@ endpoint.functions.downloadFileFromEndpoint = (endpointRequest) => {
             endpoint.events.send('onDownloadComplete', res.toString(), endpointRequest.id)
         }
     );
-    return { msg: 'File [' + file.id + '] is being downloaded.' }
+    return { msg: 'File [' + file.id + '] is being downloaded and the processing will be made asynchronously. An event will be fired when the download is complete.' }
 };
 
-endpoint.functions.uploadFileFromEndpoint = (endpointRequest) => {
-    const fileUrl = 'https://jsoncompare.org/LearningContainer/SampleFiles/PDF/sample-pdf-with-images.pdf'; 
-    endpoint.httpModule.get(fileUrl).then( res => {
-                endpoint.files.upload('somefile.pdf', res.data)
-                    .then((fileResponse) => {
-                        endpoint.events.send('onUploadComplete', fileResponse, endpointRequest.id);
-                    });
-        }).catch( error => {
-            endpoint.logger.error('Couldn\'t download the file from ['+fileUrl+'].',error);
-        });
-
-    return { msg: 'file is being downloaded' }
+endpoint.functions.syncDownloadFileFromEndpoint = async (endpointRequest) => {
+    const file = endpointRequest.params;
+    var fileResponse = await endpoint.files.download(file.id);
+    endpoint.logger.info('File download has completed!');
+    //In this case we return res.toString() because we know the file being downloaded is a .txt. Its not recommended to return the plain buffer to the platform.
+    return { fileContents: fileResponse.toString() }
 };
 
+endpoint.functions.asyncUploadFileFromEndpoint = (endpointRequest) => {
+    const fileUrl = 'https://jsoncompare.org/LearningContainer/SampleFiles/PDF/sample-pdf-with-images.pdf';
+    //We download the dummy file from an HTTP request
+    endpoint.httpModule.get(fileUrl).then(
+        (downloadResponse) => {
+            //And upload it to the platform
+            endpoint.files.upload('somefile.pdf', downloadResponse.data).then(
+                (fileInfo) => {
+                    //In this case, the info will be sent asynchronously via events
+                    endpoint.events.send('onUploadComplete', fileInfo, endpointRequest.id);
+                }
+            ).catch(
+                (err) => {
+                    endpoint.logger.error('Couldn\'t upload the file to platform.', err);
+                }
+            );
+        }
+    ).catch(
+        (err) => {
+            endpoint.logger.error('Couldn\'t download the file from [' + fileUrl + '].', err);
+        }
+    );
 
-endpoint.functions.executeScript = (endpointRequest) => {
+    return { msg: 'A file will be downloaded and then uploaded to the platform. This processing will be made asynchronously. An event will be fired when the download/upload is complete.' }
+};
+
+endpoint.functions.syncUploadFileFromEndpoint = async (endpointRequest) => {
+    const fileUrl = 'https://jsoncompare.org/LearningContainer/SampleFiles/PDF/sample-pdf-with-images.pdf';
+    try {
+        //We download the dummy file from an HTTP request
+        var downloadResponse = await endpoint.httpModule.get(fileUrl);
+    } catch (error) {
+        endpoint.logger.error('Couldn\'t download the file from [' + fileUrl + '].', error);
+    }
+    //And upload it to the platform
+    var fileInfo = await endpoint.files.upload('somefile.pdf', downloadResponse.data);
+    //The info is returned to the app synchronously 
+    return fileInfo;
+};
+
+endpoint.functions.executeScript = async (endpointRequest) => {
     const { scriptName, parameters } = endpointRequest.params;
 
-    const res = endpoint.scripts.execute(scriptName, parameters);
-    return {msg: 'Script ['+scriptName+'] will be executed shortly in a separate job',response: res};
+    const res = await endpoint.scripts.execute(scriptName, parameters);
+    return { msg: 'Script [' + scriptName + '] was executed succesfully', response: res };
 }
 
 //////////////////////////////////////////////
@@ -147,19 +177,14 @@ endpoint.functions.executeScript = (endpointRequest) => {
 endpoint.webServices.webhooks = {
     method: 'POST',
     path: '/',
-    handler: function (req, res) {
-        let body;
-        try {
-            body = req.body;
-        } catch (err) {
-            throw new Error('Body must be valid JSON');
-        }
+    handler: async (req, res) => {
+        let body = req.body
 
         // send event to app
-        endpoint.events.send('inboundEvent', body);
+        let someResponseFromApp = await endpoint.events.sendSync('inboundEvent', body);
 
         // this is what the webhook caller receives as response
-        res.send({ status: 'ok' });
+        res.send(someResponseFromApp);
     }
 }
 
